9f986f7dbfe9bcce189ee0ace62a8735a1635dda
Haopeng Song <haopengs@usc.edu>
2019-04-17 23:42:46 -0700

    Initial Commit

---
+++ ebayProductSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ebayProductSearch
+//
+//  Created by Haopeng Song on 4/17/19.
+//  Copyright © 2019 Haopeng Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
