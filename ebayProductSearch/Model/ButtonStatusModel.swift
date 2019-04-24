8dccd9a72f709dbc3ec2bc36a52457b1129c7253
Haopeng Song <haopengs@usc.edu>
2019-04-24 01:31:05 -0700

    15%

---
+++ ebayProductSearch/Model/ButtonStatusModel.swift
@@ -0,0 +1,16 @@
+//
+//  ButtonStatusModel.swift
+//  ebayProductSearch
+//
+//  Created by Haopeng Song on 4/23/19.
+//  Copyright © 2019 Haopeng Song. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConditionButtonStatusModel {
+    var newChecked: Int = 0
+    var usedChecked: Int = 0
+    var unspecChecked: Int = 0
+}
---
+++ ebayProductSearch/Model/userInput.swift
@@ -0,0 +1,9 @@
+//
+//  userInput.swift
+//  ebayProductSearch
+//
+//  Created by Haopeng Song on 4/23/19.
+//  Copyright © 2019 Haopeng Song. All rights reserved.
+//
+
+import Foundation
